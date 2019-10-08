35c09e0a38235251057decf60fa1ab496cf5a786
Jose Manuel <manuelvaldez8642@gmail.com>
2019-10-08 09:31:59 -0500

    Creación de controladores
    Mesas
    Meseros
    Inventario

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('/mesas', 'MesasController');
+Route::resource('/inventario', 'InventarioController');
+Route::resource('/meseros', 'MeserosController');
