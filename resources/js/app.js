685d0afe4ee9d47cb577ce62701a94c9d753775a
Jose Manuel <manuelvaldez8642@gmail.com>
2019-10-14 08:52:15 -0500

    First designer commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+
+//import vuetify
+import Vuetify from "vuetify";
+import 'vuetify/dist/vuetify.min.css';
+
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify()
+});
