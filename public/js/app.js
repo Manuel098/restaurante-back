03195a9233e4b9052a2056c8b995fc9a45e2a04f
Javier Correa Cepeda <javier_cc96@hotmail.com>
2019-11-17 12:51:13 -0600

    rutas componentes

--- public/js/app.js
+++ public/js/app.js
@@ -93149,8 +93149,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Usuario\Documents\7o Cuatri\Pruebas SW\Restaurante\restaurante-back\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Usuario\Documents\7o Cuatri\Pruebas SW\Restaurante\restaurante-back\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\restaurante-back\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\restaurante-back\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
