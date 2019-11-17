40b459caeb15a8f1c918d3aa0204ad9c07c26b25
Jose Manuel <manuelvaldez8642@gmail.com>
2019-11-17 16:23:18 -0600

    FTotla model

--- app/EfecTotal.php
+++ app/EfecTotal.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EfecTotal extends Model
 {
-    //
+    use SoftDeletes;
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $table = "FTotal";
+    
+    protected $fillable = [
+        'descripcion', 'cantidad'
+    ];
 }
