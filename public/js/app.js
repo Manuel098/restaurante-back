559a4a442b420d2fc941d9ef3b024e4190d2fcb6
Dave <alexruedarivas@gmail.com>
2019-11-17 16:17:36 -0600

    sin log in

--- public/js/app.js
+++ public/js/app.js
@@ -2021,8 +2021,8 @@ __webpack_require__.r(__webpack_exports__);
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var jspdf__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! jspdf */ "./node_modules/jspdf/dist/jspdf.min.js");
-/* harmony import */ var jspdf__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(jspdf__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var jspdf__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! jspdf */ "./node_modules/jspdf/dist/jspdf.min.js");
+/* harmony import */ var jspdf__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(jspdf__WEBPACK_IMPORTED_MODULE_1__);
 //
 //
 //
@@ -2146,7 +2146,7 @@ __webpack_require__.r(__webpack_exports__);
       return total;
     },
     terminar: function terminar() {
-      var doc = new jspdf__WEBPACK_IMPORTED_MODULE_0___default.a(); //var img = new Image();
+      var doc = new jspdf__WEBPACK_IMPORTED_MODULE_1___default.a(); //var img = new Image();
       //img.src = 'logo.jpeg';
       //doc.addImage(img, 'jpeg', 20, 20,185,120);
 
@@ -39624,16 +39624,6 @@ var render = function() {
         "v-flex",
         { attrs: { xs12: "" } },
         [
-          _c("Login", {
-            attrs: { dialogShared: _vm.initialDialog },
-            on: { log: _vm.setInitial }
-          }),
-          _vm._v(" "),
-          _c("Signin", {
-            attrs: { signShared: _vm.initialSign },
-            on: { sign: _vm.setSign }
-          }),
-          _vm._v(" "),
           _c(
             "v-toolbar",
             { attrs: { dark: "", fixed: "" } },
@@ -39645,39 +39635,7 @@ var render = function() {
                 1
               ),
               _vm._v(" "),
-              _c("div", { staticClass: "flex-grow-1" }),
-              _vm._v(" "),
-              _c(
-                "v-toolbar-items",
-                [
-                  _c(
-                    "v-btn",
-                    {
-                      attrs: { text: "" },
-                      on: {
-                        click: function($event) {
-                          return _vm.handleLogin()
-                        }
-                      }
-                    },
-                    [_vm._v("Log in")]
-                  ),
-                  _vm._v(" "),
-                  _c(
-                    "v-btn",
-                    {
-                      attrs: { text: "" },
-                      on: {
-                        click: function($event) {
-                          return _vm.handleSignin()
-                        }
-                      }
-                    },
-                    [_vm._v("Sign in")]
-                  )
-                ],
-                1
-              )
+              _c("div", { staticClass: "flex-grow-1" })
             ],
             1
           ),
@@ -95033,8 +94991,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\restaurante-back\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\restaurante-back\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\David\Desktop\somebody\restaurante-back\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\David\Desktop\somebody\restaurante-back\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
